fee62d16a3a2f915d50259383b8f871b9b5bb674
ippaivacode <gustavoippaiva@gmail.com>
2025-01-24 22:07:58 +0000

    Melhorei o uso do scanf e printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,51 +1,45 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {  //declaração de variáveis.
-    char estado[20];
-    char codigo[20];       
-    char nome[50];
-    int populacao;
-    float area;
-    float pib;
-    int pontosturisticos;
-    
-    // Cadastro das Cartas:
-    printf("Digite o estado da cidade: "); //solicitando o estado para o usuário.
-    scanf("%19s", estado); //usando o scanf para guardar a informação para poder imprimir depois
-    //usei o "%19s" para limitar a entrada para nao causar estouro de memória.
-
-    printf("Digite o código da carta: ");//solicitação do código da carta pro usuário.
-    scanf("%19s", codigo);//usando o (%19s) pelo mesmo motivo da variavel "estado"
-    
-    printf("Digite o nome da cidade: ");//solicitando o nome da cidade pro usuário.
-    scanf("%s", &nome);//scanf para memorizar a informação.
-
-    printf("Digite a população da cidade: ");//solicitação da população da cidade.
-    scanf("%f", &populacao);//scanf para memorizar a informação.
-
-    printf("Digite a área da cidade: ");//solicitação da área da cidade.
-    scanf("%f", &area);//scanf para memorizar a informação.
-
-    printf("Digite o numero de pontos turísticos: ");//solicitação da quantidade de pontos turísticos.
-    scanf("%f", &pontosturisticos);//scanf para memorizar a informação.
-    
-    printf("Digite o pib da cidade: ");//solicitação do pib percapita da cidade.
-    scanf("%f", &pib);//scanf para memorizar a informação.
+int main() {
+                                     //Declarando variáveis:
+                    
+    char estado;                // Estado (letra A-H)
+    int cidade;                 // Número da cidade (1-4) 
+    float populacao;            // População da cidade
+    float area;                 // Área da cidade em km²
+    int pontos_turisticos;      // Número de pontos turísticos
+    float pib;                  // PIB da cidade em milhões   
     
 
-    // Exibição dos Dados das Cartas: //usando o "printf" para imprimir os dados coletados anteriormente.
-    printf("Codigo : %s \n",codigo);
-    printf("Nome : %s \n", nome);
-    printf("População: %d \n", populacao);
-    printf("Área: %.2f km² \n", area);//usei o "%.2f" para poder adicionar a grandeza, no caso em (km²).
-    printf("Pontos turísticos: %d \n", pontosturisticos);
-    printf("PIB: %.2f Bilhões de reais \n", pib);//usei o "%.2f" para poder adicionar a grandeza, no caso (Bilhões de reais)
+                                     //Cadastro das Cartas:
+    //printf para imprimir as perguntas.
+    //scanf para armazenar os dados inseridos.
+    printf("Digite a letra do estado (A-H):\n");        //Solicitando a letra do estado para o cadastro da carta.
+    scanf("%c", &estado);                               
+    
+    printf("Digite o número da cidade (1-4):\n");       //Solicitando o numero da cidade de 1 a 4.
+    scanf("%d", &cidade);
+
+    printf("Digite a população da cidade(Milhões):\n"); //Solicitando a população da cidade.
+    scanf("%f", &populacao);
+    
+    printf("Digite a área da cidade em km²:\n");        //Solicitação da área da cidade(em km²).
+    scanf("%f", &area);                               
+
+    printf("Digite o numero de pontos turísticos:\n");  //Solicitação da quantidade de pontos turísticos.
+    scanf("%d", &pontos_turisticos);                  
+    
+    printf("Digite o pib da cidade(Bilhões):\n");       //Solicitação do PIB da cidade.
+    scanf("%f", &pib);                                
+    
+
+                                // Exibição dos Dados das Cartas: 
+    printf("Estado (A-H): %c \n",estado);               // "printf" sendo usado para imprimir os dados da carta cadastrada.
+    printf("Cidade (1-4): %d \n", cidade);
+    printf("População: %.2fmilhões \n", populacao);     //"%.2f" usado para adicionar as grandezas(milhões,km²,reais)
+    printf("Área: %.2f km² \n", area);                 
+    printf("Pontos turísticos: %d \n", pontos_turisticos);
+    printf("PIB: %.2f Bilhões de Reais \n", pib);
     
 
     return 0;
